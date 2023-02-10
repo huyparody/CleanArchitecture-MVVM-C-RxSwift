64493fdc45679a9c4bbbf22c7f2a7bb0eaf033fa
Huy Trinh Duc <huytd@educa.vn>
2023-02-10 22:05:45 +0700

    Refactor setup script & project structure

--- setup.sh
+++ setup.sh
@@ -1,11 +1,12 @@
+#!/bin/bash
+PROJECT_NAME="CleanArchitecture-MVVM-C-RxSwift"
+
 if brew ls --versions xcodegen > /dev/null; then
   # The package is installed
   echo "XcodeGen is installed..."
-  rm -rf CleanArchitecture-MVVM-C-RxSwift.xcodeproj && xcodegen
-  echo "\nDone, now open the .xcodeprj file.\n"
+  rm -rf $PROJECT_NAME.xcodeproj && xcodegen && open $PROJECT_NAME.xcodeproj
 else
   # The package is not installed
   echo "XcodeGen is not installed...\nInstalling XcodeGen..."
-  brew install xcodegen && rm -rf CleanArchitecture-MVVM-C-RxSwift.xcodeproj && xcodegen && brew list
-  echo "\nDone, now open the .xcodeprj file.\n"
+  brew install xcodegen && rm -rf $PROJECT_NAME.xcodeproj && xcodegen && open $PROJECT_NAME.xcodeproj
 fi
